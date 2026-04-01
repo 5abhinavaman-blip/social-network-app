7fa3af02af34078807ca57b7e31ba4179c68c727
5abhinavaman-blip <5abinavaman@gmail.com>
2026-04-02 00:07:51 +0530

    db connection issue resolved

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const db = mysql.createConnection({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
+    password: process.env.DB_PASSWORD || '1234',
     database: process.env.DB_NAME || 'social_app'
 });
 
@@ -16,4 +16,6 @@ db.connect(err => {
     console.log("MySQL Connected...");
 });
 
+
+console.log("PASSWORD:", process.env.DB_PASSWORD);
 module.exports = db;
